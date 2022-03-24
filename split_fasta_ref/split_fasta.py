500d94abc3d35433502d0e7a516d7a62a22f6be9
MartinSchmitz95 <schmitz.kessenich@gmail.com>
2022-03-24 11:39:13 +0800

    add function to divide fastas

--- split_fasta_ref/split_fasta.py
+++ split_fasta_ref/split_fasta.py
@@ -5,15 +5,31 @@ import os
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--data', type=str, help='Path to fasta file')
+    parser.add_argument('--divide', type=int, default=0, help='length of chunks')
     args = parser.parse_args()
 
+    if args.divide > 0:
+        if not os.path.exists("divided_fastas"):
+            os.makedirs("divided_fastas")
 
-    if not os.path.exists("single_fastas"):
-        os.makedirs("single_fastas")
+        fasta_sequences = SeqIO.parse(open(args.data), 'fasta')
+        for fasta in fasta_sequences:
+            name, sequence = fasta.id, fasta.seq
+            amount_of_files = len(fasta.seq)//args.divide
 
-    fasta_sequences = SeqIO.parse(open(args.data), 'fasta')
-    for fasta in fasta_sequences:
-        name, sequence = fasta.id, fasta.seq
-        with open(os.path.join("single_fastas", name + ".fasta"), "w") as output_handle:
-            SeqIO.write(fasta, output_handle, "fasta")
-        print(name)
+            for f in range(amount_of_files):
+                sequence = fasta.seq[f*args.divide:(f+1)*args.divide]
+                with open(os.path.join("divided_fastas", name + "_" + str(f) + ".fasta"), "w") as output_handle:
+                    SeqIO.write(fasta, output_handle, "fasta")
+                print(name + "_" + str(f))
+
+    else:
+        if not os.path.exists("single_fastas"):
+            os.makedirs("single_fastas")
+
+        fasta_sequences = SeqIO.parse(open(args.data), 'fasta')
+        for fasta in fasta_sequences:
+            name, sequence = fasta.id, fasta.seq
+            with open(os.path.join("single_fastas", name + ".fasta"), "w") as output_handle:
+                SeqIO.write(fasta, output_handle, "fasta")
+            print(name)
