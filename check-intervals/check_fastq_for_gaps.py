593b502b61bb0a6cdae91d7843b1dead26697075
MartinSchmitz95 <schmitz.kessenich@gmail.com>
2022-04-11 15:00:44 +0800

    check both strands

--- check-intervals/check_fastq_for_gaps.py
+++ check-intervals/check_fastq_for_gaps.py
@@ -14,11 +14,12 @@ def load_reads(path):
             start_index = 0
         else:
             print("something went wrong")
-        read = [int(des[start_index + 2][6:-1]), int(des[start_index + 3][4:])]
-        if des[start_index + 1][-2] == '+':
+        if des[start_index + 1][-2] == '+' or start_index==0:
+            read = [int(des[start_index + 2][6:-1]), int(des[start_index + 3][4:])]
             pos_reads.append(read)
         elif des[start_index + 1][-2] == '-':
-            pass
+            read = [int(des[start_index + 3][4:]), int(des[start_index + 2][6:-1])]
+            pos_reads.append(read)
         else:
             print("error wrong strand symbol")
     return pos_reads
