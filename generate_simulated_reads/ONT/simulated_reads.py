37d71cf04d768a7f9a73c0b5d4fb68978bcf6ef8
IG2501 <65847417+IG2501@users.noreply.github.com>
2022-06-27 11:26:39 +0800

    Update simulated_reads.py

--- generate_simulated_reads/ONT/simulated_reads.py
+++ generate_simulated_reads/ONT/simulated_reads.py
@@ -22,3 +22,21 @@ def coverage(input_path,args):
     #default is 1000 reads
     n = (total_bases)/avg_length 
     return n
+
+#characterize and simulate using NanoSim
+def simulate(input_path,data_path,name,args):
+    
+    n = coverage(input_path,args)
+    
+    for i in range(0,args.runs):
+        print(f'Processing for dataset:{i}')
+        
+        #characterization phase--> read profile
+        global number_aligned, number_unaligned, number_aligned_l
+        simulator.read_profile(ref_g=input_path , number_list=[round(n)], model_prefix="NanoSim/human_NA12878_DNA_FAB49712_albacore/training",
+                          per=args.perfect, mode="genome", strandness=None, dna_type="linear", chimeric=False)
+        #simulate 
+        simulator.set_globals()
+        #name of the output file chromosome_name/chromosome_name_{i}.fasta
+        simulator.simulation(mode="genome", out=os.path.join(os.path.abspath(data_path),f'{name}_{i}'), dna_type="linear", per=args.perfect, kmer_bias=None, basecaller="albacore", read_type="DNA", max_l=float("inf"), min_l=50, num_threads=1,
+                           fastq=False, chimeric=False)
