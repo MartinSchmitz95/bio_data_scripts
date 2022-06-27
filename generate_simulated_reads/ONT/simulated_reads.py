60f8f2df09c9fa096b1adafbde75da433e51a1e6
IG2501 <65847417+IG2501@users.noreply.github.com>
2022-06-27 11:25:39 +0800

    Update simulated_reads.py

--- generate_simulated_reads/ONT/simulated_reads.py
+++ generate_simulated_reads/ONT/simulated_reads.py
@@ -1,1 +1,8 @@
-
+from NanoSim.src import simulator
+from Bio import SeqIO
+from Bio.SeqRecord import SeqRecord
+from Bio.Seq import Seq
+from Bio.SeqIO.FastaIO import SimpleFastaParser
+import argparse
+import os
+import shutil
